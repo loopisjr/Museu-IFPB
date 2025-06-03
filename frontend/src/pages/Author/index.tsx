05b9af4f37f0a9ff4cbae311ca17220eb65ad29f
JoseHenrique1 <jh4832184@gmail.com>
2025-06-03 06:59:16 -0300

    view: adicionando base das paginas

---
+++ frontend/src/pages/Author/index.tsx
@@ -0,0 +1,8 @@
+export default function Author() {
+  return (
+    <>
+      Author
+    </>
+  )
+}
+
---
+++ frontend/src/pages/City/index.tsx
@@ -0,0 +1,8 @@
+export default function City() {
+  return (
+    <>
+      City
+    </>
+  )
+}
+
