c36eee6c53d531d59e1c7ed74b08971373653e16
JoseHenrique1 <jh4832184@gmail.com>
2025-06-07 18:59:23 -0300

    view: adicionando pagina de autor

--- frontend/src/pages/Author/index.tsx
+++ frontend/src/pages/Author/index.tsx
@@ -1,8 +1,32 @@
+import style from "./author.module.css"
+import facebook from "../../assets/icons/facebook-color.png"
+import instagram from "../../assets/icons/instagram-color.png"
+import linkedin from "../../assets/icons/linkedin-color.png"
+
 export default function Author() {
   return (
-    <>
-      Author
-    </>
+    <div className={`${style.container} container`}>
+      <img src="https://images.pexels.com/photos/2513993/pexels-photo-2513993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Autor" />
+      <div className={style.content}>
+        <div className={style.header}>
+          <h1 className={style.authorName}>José Sérgio Aristides Lira</h1>
+          <p className={style.authorProfession}>PESQUISADOR E ESTUDANTE</p>
+        </div>
+        <div>
+          <p className={style.authorBody}>
+            Meu nome é José Sérgio Aristides Lira. Sou graduado em História pela Universidade Federal de Campina Grande (UFCG), com especialização no Ensino de História pela Faculdade Única de Ipatinga. Sou também Técnico em Segurança do Trabalho pelo Instituto Federal da Paraíba (IFPB) – Campus Cajazeiras e mestrando em Educação Profissional e Tecnológica (ProfEPT) no Instituto Federal do Sertão Pernambucano – Campus Salgueiro.
+Trabalhei por sete anos na Companhia de Águas e Esgotos da Paraíba (CAGEPA) e, desde 2016, sou servidor técnico-administrativo em Educação no Instituto Federal da Paraíba, tendo atuado nos campi Catolé do Rocha e Cajazeiras.
+          </p>
+        </div>
+        <div className={style.boxIcons}>
+
+          <img className={style.icon} src={facebook} alt="Icon facebook" />
+          <img className={style.icon} src={instagram} alt="Icon instagram" />
+          <img className={style.icon} src={linkedin} alt="Icon linkedin " />
+
+        </div>
+      </div>
+    </div>
   )
 }
 
